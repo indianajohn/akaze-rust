4debac84f2946094482780f324eb71e4dcb0b8a8
John <johnstalbaum@gmail.com>
2018-09-10 01:15:20 -0700

    Added integration test.

---
+++ tests/integration-test.rs
@@ -0,0 +1,20 @@
+
+extern crate akaze;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn locate_test_data() -> PathBuf {
+    let exe_path = ::std::env::current_exe().unwrap();
+    let mut parent_path = exe_path.parent().unwrap().to_owned();
+    parent_path.push("../../../test-data/akaze-test-data");
+    parent_path
+}
+
+#[test]
+fn extract_features() {
+    let mut test_image_path = locate_test_data();
+    test_image_path.push("1.jpg");
+    // TODO: temp dir
+    let output_path = Path::new("output.png");
+    akaze::extract_features(test_image_path, output_path.to_owned());
+}
