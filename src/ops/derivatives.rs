0cbfbd68e9acc750fef0e4608a2ba52dddb59c61
John <johnstalbaum@gmail.com>
2018-09-17 01:10:30 -0700

    Derivatives must be computed both ways.

--- src/ops/derivatives.rs
+++ src/ops/derivatives.rs
@@ -18,23 +18,35 @@ pub fn sqrt_squared(image_1: &GrayFloatImage, image_2: &GrayFloatImage) -> GrayF
     result
 }
 
-fn scharr_horizontal(image: &GrayFloatImage) -> GrayFloatImage {
-    let kernel: [f32; 9] = [-3f32, 0f32, 3f32, -10f32, 0f32, 10f32, -3f32, 0f32, 3f32];
+fn scharr_horizontal(image: &GrayFloatImage, scale_factor: f32) -> GrayFloatImage {
+    let x = 3f32;
+    let y = 10f32;
+    let kernel: [f32; 9] = [
+        -x / scale_factor, 0f32 / scale_factor, x / scale_factor, 
+        -y / scale_factor, 0f32 / scale_factor, y / scale_factor,
+        -x / scale_factor, 0f32 / scale_factor, x / scale_factor];
     image::imageops::filter3x3(image, &kernel)
 }
 
-fn scharr_vertical(image: &GrayFloatImage) -> GrayFloatImage {
-    let kernel: [f32; 9] = [-3f32, -10f32, -3f32, 0f32, 0f32, 0f32, 3f32, 10f32, 3f32];
+fn scharr_vertical(image: &GrayFloatImage, scale_factor: f32) -> GrayFloatImage {
+    let x = 3f32;
+    let y = 10f32;
+    let kernel: [f32; 9] = [
+        -x / scale_factor, -y / scale_factor, -x / scale_factor,
+        0f32 / scale_factor, 0f32 / scale_factor, 0f32 / scale_factor,
+        x / scale_factor, y / scale_factor, x / scale_factor];
     image::imageops::filter3x3(image, &kernel)
 }
 
 pub fn scharr(image: &GrayFloatImage, x_order: bool, y_order: bool) -> GrayFloatImage {
     if x_order && y_order {
-        sqrt_squared(&scharr_vertical(&image), &scharr_horizontal(&image))
+        let horizontal = sqrt_squared(&scharr_horizontal(&image, 1f32), &scharr_horizontal(&image, -1f32));
+        let vertical = sqrt_squared(&scharr_vertical(&image, 1f32), &scharr_vertical(&image, -1f32));
+        sqrt_squared(&vertical, &horizontal)
     } else if x_order {
-        scharr_horizontal(image)
+        sqrt_squared(&scharr_horizontal(&image, 1f32), &scharr_horizontal(&image, -1f32))
     } else if y_order {
-        scharr_vertical(image)
+        sqrt_squared(&scharr_vertical(&image, 1f32), &scharr_vertical(&image, -1f32))
     } else {
         GrayFloatImage::new(image.width(), image.height())
     }
