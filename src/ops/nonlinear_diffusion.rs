3739ae9f7bc9523a24c6ce655e9e0c0072b82953
John <johnstalbaum@gmail.com>
2018-09-28 00:34:44 -0700

    Itertors seem to be a lot faster.

--- src/ops/nonlinear_diffusion.rs
+++ src/ops/nonlinear_diffusion.rs
@@ -13,19 +13,60 @@ pub fn calculate_step(evolution_step: &mut EvolutionStep, step_size: f64) {
     let Ld: &mut GrayFloatImage = &mut evolution_step.Lt;
     let c: &GrayFloatImage = &evolution_step.Lflow;
     let Lstep: &mut GrayFloatImage = &mut evolution_step.Lstep;
+    let w = Lstep.width();
+    let h = Lstep.height();
 
     // Diffusion all the image except borders
-    for y in 1..(Lstep.height() - 1) {
-        for x in 1..(Lstep.width() - 1) {
-            let x_pos = eval(c, Ld, x, y, 0, 1, 1, 0, 0, 0, 0, 0);
-            let x_neg = eval(c, Ld, x, y, -1, 0, 0, -1, 0, 0, 0, 0);
-            let y_pos = eval(c, Ld, x, y, 0, 0, 0, 0, 0, 1, 1, 0);
-            let y_neg = eval(c, Ld, x, y, 0, 0, 0, 0, -1, 0, 0, -1);
-            Lstep.put(
-                x,
-                y,
-                0.5 * (step_size as f32) * (x_pos - x_neg + y_pos - y_neg),
-            );
+    for y in 1..(h - 1) {
+        let mut Ld_yn = Ld.buffer.iter(); 
+        let mut Ld_yn_i = Ld_yn.nth(w * (y - 1) + 1).unwrap();
+
+        let mut Ld_yp = Ld.buffer.iter(); 
+        let mut Ld_yp_i = Ld_yp.nth(w * (y + 1) + 1).unwrap();
+
+        let mut Ld_xn = Ld.buffer.iter();
+        let mut Ld_xn_i = Ld_xn.nth(w * y + 0).unwrap();
+
+        let mut Ld_x  = Ld.buffer.iter();
+        let mut Ld_x_i = Ld_x.nth(w * y + 1).unwrap();
+
+        let mut Ld_xp = Ld.buffer.iter();
+        let mut Ld_xp_i = Ld_xp.nth(w * y + 2).unwrap();
+
+        let mut c_yn = c.buffer.iter();
+        let mut c_yn_i = c_yn.nth(w * (y - 1) + 1).unwrap();
+
+        let mut c_yp = c.buffer.iter();
+        let mut c_yp_i = c_yp.nth(w * (y + 1) + 1).unwrap();
+
+        let mut c_xn = c.buffer.iter();
+        let mut c_xn_i = c_xn.nth(w * y + 0).unwrap();
+
+        let mut c_x  = c.buffer.iter();
+        let mut c_x_i = c_x.nth(w * y + 1).unwrap();
+
+        let mut c_xp = c.buffer.iter();
+        let mut c_xp_i = c_xp.nth(w * y + 2).unwrap();
+
+        let slice = &mut Lstep.buffer[(w * y+1)..(w * y + w - 1)];
+        for Lstep_x_i in slice.iter_mut() {
+            let x_pos = (c_x_i + c_xp_i) * (Ld_xp_i - Ld_x_i);
+            let x_neg = (c_xn_i + c_x_i) * (Ld_x_i - Ld_xn_i);
+            let y_pos = (c_x_i + c_yp_i) * (Ld_yp_i - Ld_x_i);
+            let y_neg = (c_yn_i + c_x_i) * (Ld_x_i - Ld_yn_i);
+            *Lstep_x_i = 0.5 * (step_size as f32) * (x_pos - x_neg + y_pos - y_neg);
+
+            c_x_i = c_x.next().unwrap();
+            c_xp_i = c_xp.next().unwrap();
+            c_xn_i = c_xn.next().unwrap();
+            c_yp_i = c_yp.next().unwrap();
+            c_yn_i = c_yn.next().unwrap();
+
+            Ld_x_i = Ld_x.next().unwrap();
+            Ld_xp_i = Ld_xp.next().unwrap();
+            Ld_xn_i = Ld_xn.next().unwrap();
+            Ld_yp_i = Ld_yp.next().unwrap();
+            Ld_yn_i = Ld_yn.next().unwrap();
         }
     }
     // First row
@@ -111,12 +152,10 @@ pub fn calculate_step(evolution_step: &mut EvolutionStep, step_size: f64) {
             );
         }
     }
-    for x in 0..Lstep.width() {
-        for y in 0..Lstep.height() {
-            let Ld_pixel = Ld.get(x, y);
-            let Lstep_pixel = Lstep.get(x, y);
-            Ld.put(x, y, Ld_pixel + Lstep_pixel);
-        }
+
+    let mut Lstep_iter = Lstep.buffer.iter();
+    for Ld_iter in Ld.buffer.iter_mut() {
+        *Ld_iter += Lstep_iter.next().unwrap();
     }
 }
 
